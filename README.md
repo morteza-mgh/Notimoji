e30e8757f7852fa65a9d340bedbb3cd815c2e77d
Morteza <morteza.mgh.job@gmail.com>
2025-01-11 23:20:14 +0330

    Init project

--- README.md
+++
@@ -1,2 +0,0 @@
-# notimoji
-Note taking app with emoji
---
+++ app/src/main/java/mgh/morteza/notimoji/MainActivity.kt
@@ -0,0 +1,47 @@
+package mgh.morteza.notimoji
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mgh.morteza.notimoji.ui.theme.NotimojiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotimojiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NotimojiTheme {
+        Greeting("Android")
+    }
+}
